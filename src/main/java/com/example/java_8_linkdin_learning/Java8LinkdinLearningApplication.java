2250721f692dd4ca520956c25e94c4ca4b42bc42
Aditya <15.adityasinha@gmail.com>
2022-07-31 16:44:41 +0530

    Initial commit

---
+++ src/main/java/com/example/java_8_linkdin_learning/Java8LinkdinLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_8_linkdin_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LinkdinLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8LinkdinLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_8_linkdin_learning/Java8LinkdinLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_8_linkdin_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8LinkdinLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
